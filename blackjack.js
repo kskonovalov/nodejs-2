8bb4f6f409fc75ff9977cdf7b55d172b852e47f6
Konstantin <picprofit@gmail.com>
2016-03-29 20:43:26 +0300

    primitive blackjack

--- blackjack.js
+++ blackjack.js
@@ -21,31 +21,56 @@ function logInFile(filename, text) {
     });
 }
 
+function whoWon(userScore, robotScore) {
+    if(userScore > robotScore)
+        gameResult = "You won!\n";
+    else if(userScore < robotScore)
+        gameResult = "Robot won!\n";
+    else gameResult = "Draw!\n";
+    return gameResult;
+}
 
-console.log("BlackJack game. You got 0 points now\n");
+console.log("BlackJack game. You and Robot have 0 points now\n");
 //BlackJack Game
 rl.setPrompt('Print "more" (or just press Enter), "done" or "exit" > ');
 rl.prompt();
-var score = 0, userScore = 0, maxScore = 21, gameResult;
+var maxScore = 21;
+var score = 0, userScore = 0, robotScore = 0, gameResult;
 var activeGame = true;
 rl.on('line', (line) => {
     switch(line) {
         case "done":
         case "exit":
-            gameResult = `You result is "${userScore}"!\n`;
+            gameResult = `You result is "${userScore}", robot result is "${robotScore}"!\n`;
+            gameResult = gameResult + whoWon(userScore, robotScore);
             activeGame = false;
             break;
         case "more":
         default:
         score = getNaturalRandom(1,10);
         userScore += score;
-        gameResult = `Added "${score}", you have "${userScore}" points now\n`;
-        if(userScore == 21) {
-            gameResult = gameResult + "YOU WON!!!";
+        gameResult = `YOU: Added "${score}", you have "${userScore}" points now\n`;
+        score = getNaturalRandom(1,10);
+        robotScore += score;
+        gameResult += `ROBOT: Added "${score}", robot have "${robotScore}" points now\n`;
+        if(userScore > maxScore && robotScore > maxScore) {
+            gameResult += "DRAW!";
             activeGame = false;
         }
-        else if(userScore > 21) {
-            gameResult = gameResult + "you lose =(";
+        else if(userScore > maxScore) {
+            gameResult += "You lose..";
+            activeGame = false;
+        }
+        else if(robotScore > maxScore) {
+            gameResult += "Robot lose!";
+            activeGame = false;
+        }
+        else if(userScore == maxScore) {
+            gameResult += "You won!!!";
+            activeGame = false;
+        }
+        else if(robotScore == maxScore) {
+            gameResult += "Robot won)";
             activeGame = false;
         }
         break;
